3bf382e225bdd55921af3357da9868675f369a78
Tom Tran <>
2023-04-04 23:12:47 +0700

    Initialize commit

---
+++ ReciMeAssignment/ReciMeAssignment/ReciMeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciMeAssignmentApp.swift
+//  ReciMeAssignment
+//
+//  Created by Tran Tuyen on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReciMeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
